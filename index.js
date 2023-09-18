da7a4a483963cc507a37523b75e29917a2ffcf99
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-09-18 09:54:27 +0300

    try fix build

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-app.use(express.static(""));
 
 app.get("/cars", (req, res) => {
   let timeout;

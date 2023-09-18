9759acf3ed8a613db7dc5d02a60d884f13f5471a
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-09-18 10:55:37 +0300

    try fix cors

--- index.js
+++ index.js
@@ -10,12 +10,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(express.static("public"));
-app.use((req, res, next) => {
-  res.header(
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
     "Access-Control-Allow-Origin",
     "https://juzt-test-client.vercel.app"
   );
-  res.header("Access-Control-Allow-Headers", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
   next();
 });
 
